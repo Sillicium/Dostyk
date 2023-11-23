6211dbb083b2a1dd0d4b35438e76be50b4a35bab
Sillicium <spotttiework@gmail.com>
2023-11-23 21:59:30 +0600

    add place holder pic & add data classes

---
+++ app/src/main/java/com/example/myapplication/data/TemporaryDataClass.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.data
+
+data class TemporaryDataClassText(
+    val title: Int
+)
+
+data class TemporaryDataClassTextAndIcon(
+    val icon: Int,
+    val title: Int,
+)
+
+data class TemporaryDataClassTextDescriptionAndIcon(
+    val icon: Int,
+    val title: Int,
+    val description: Int
+)
