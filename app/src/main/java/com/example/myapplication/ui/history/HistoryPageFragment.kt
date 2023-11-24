80a1d4a59fe9821c0133b667b6aea1b647b5ff07
Sillicium <102834057+Sillicium@users.noreply.github.com>
2023-11-24 09:15:57 +0600

    add layat manager into recycle view and create history adapter

--- app/src/main/java/com/example/myapplication/ui/history/HistoryPageFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/history/HistoryPageFragment.kt
@@ -5,11 +5,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dostyk.utils.fragment_utils.BindingFragment
+import com.example.myapplication.R
+import com.example.myapplication.data.TemporaryDataClassOneElement
+import com.example.myapplication.data.TemporaryDataClassThreeElement
 import com.example.myapplication.databinding.FragmentHistoryPageBinding
 
 class HistoryPageFragment : BindingFragment<FragmentHistoryPageBinding>(
     FragmentHistoryPageBinding::inflate
 ) {
+
+    private val OneElementAdaptersData = listOf(
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+    )
+
+    private val SecondElementAdaptersData = listOf(
+        R.drawable.ic_google,
+        R.string.order_history,
+        R.string.order_history,
+        R.string.order_history,
+        R.string.order_history,
+    )
+
+    private val ThirdElementAdaptersData = listOf(
+        R.drawable.ic_google,
+        R.string.about_company,
+        R.string.about_company,
+        R.string.about_company,
+        R.string.about_company,
+    )
+
+    var index = 0
+
+    private val threeElement = TemporaryDataClassThreeElement(OneElementAdaptersData[index],
+        SecondElementAdaptersData[index],
+        ThirdElementAdaptersData[index])
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,7 +65,15 @@ class HistoryPageFragment : BindingFragment<FragmentHistoryPageBinding>(
 
     private fun setupView(){
 
+        val historyAdapter = PaymentHistoryAdapter(requireContext()).apply {
+            setItems(listOf(threeElement) )
+        }
 
+        binding.notificationDetailsList.adapter = historyAdapter
+
+        binding.tvOrderHistoryPageText.setOnClickListener {
+            index++
+        }
     }
     private fun setupBtn(){
 
