6ab78efa3ea2e0295473aed945c44ffb7263a29f
Sillicium <102834057+Sillicium@users.noreply.github.com>
2023-11-24 08:51:27 +0600

    create all home page adapters

--- app/src/main/java/com/example/myapplication/ui/home/HomePageFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomePageFragment.kt
@@ -1,19 +1,43 @@
 package com.example.myapplication.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dostyk.utils.fragment_utils.BindingFragment
 import com.example.myapplication.R
-import com.example.myapplication.databinding.FragmentHistoryPageBinding
+import com.example.myapplication.data.TemporaryDataClassOneElement
+import com.example.myapplication.data.TemporaryDataClassTwoElement
 import com.example.myapplication.databinding.FragmentHomePageBinding
+import com.example.myapplication.ui.home.adapters.HomePage1Adapter
+import com.example.myapplication.ui.home.adapters.NewPlacesAdapter
+import com.example.myapplication.ui.home.adapters.TopBrandsAdapter
 
 
 class HomePageFragment : BindingFragment<FragmentHomePageBinding>(
     FragmentHomePageBinding::inflate
 ) {
+
+    private val OneElementAdaptersData = listOf(
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+        R.drawable.ic_google,
+    )
+
+    private val oneElement = TemporaryDataClassOneElement(OneElementAdaptersData[4])
+
+    private val TwoElementAdaptersData = listOf(
+        R.drawable.place_holder_image to R.string.about_company,
+        R.drawable.place_holder_image to R.string.about_company,
+        R.drawable.place_holder_image to R.string.about_company,
+        R.drawable.place_holder_image to R.string.about_company,
+        R.drawable.place_holder_image to R.string.about_company,
+        R.drawable.place_holder_image to R.string.about_company,
+    ).map { TemporaryDataClassTwoElement(it.first, it.second) }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +59,24 @@ class HomePageFragment : BindingFragment<FragmentHomePageBinding>(
 
     private fun setupView(){
         binding.fragmentTopBarLayout.searchInputLayout.isHintEnabled = false
+
+
+        val topBrandsAdapter = TopBrandsAdapter(requireContext()).apply {
+            setItems(TwoElementAdaptersData)
+        }
+
+        val newPlacesAdapter = NewPlacesAdapter(requireContext()).apply {
+            setItems(TwoElementAdaptersData)
+        }
+
+        val homePage1Adapter = HomePage1Adapter(requireContext()).apply {
+            setItems(listOf(oneElement))
+        }
+        binding.recyclerView.adapter = homePage1Adapter
+
+        binding.topBrandsLayout.topBrandsList.adapter = topBrandsAdapter
+
+        binding.newPlacesLayout.newPlacesList.adapter = newPlacesAdapter
 
     }
     private fun setupBtn(){
--- app/src/main/java/com/example/myapplication/ui/home/adapters/TopBrandsAdapter.kt
+++ app/src/main/java/com/example/myapplication/ui/home/adapters/TopBrandsAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.history
+package com.example.myapplication.ui.home.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
